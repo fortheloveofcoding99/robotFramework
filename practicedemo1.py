419d366199b17b7d87fe8273549e1ceab3a0fb8c
Som <94239450+fortheloveofcoding99@users.noreply.github.com>
2022-04-02 08:56:19 +0530

    Add files via upload

---
+++ practicedemo1.py
@@ -0,0 +1,15 @@
+
+
+x, y, z = 2, 3.5, "Python"
+
+print("{} {} {} {} {} {}".format("Integer is", x, "Float is", y, "This is concatenate in lang", z))
+
+print("Is of", type(y))
+
+list1 = ["ersten Element", 2, 3.14, 6.023, "zweiten Element"]
+print(list1)
+list1.append("das funfth element")
+print(list1)
+list1.insert(2, "kuche")
+print(list1)
+
