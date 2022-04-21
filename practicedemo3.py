5094aa64f0a0610d34716b5f3d54803a462ccaa0
Som <94239450+fortheloveofcoding99@users.noreply.github.com>
2022-04-21 18:43:13 +0530

    Add files via upload

---
+++ practicedemo3.py
@@ -0,0 +1,18 @@
+
+
+list1 = [6, "Bewerbung", "Uhr", "Geshiste", "Aufgaben", "Mein Leid"]
+print(list1)
+list1.pop()
+print(list1)
+list1.remove(list1[2])
+list1.append("termin")
+print(list1)
+
+tuple1 =("rot", "grun", "blau", 23)
+print(tuple1)
+
+str1 = "Deutsch ist Einfach"
+set1 = set(str1)
+print(set1)
+
+
