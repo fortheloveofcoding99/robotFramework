c97a90f44dd5f55f117e11796e844bab804d3e08
Som <94239450+fortheloveofcoding99@users.noreply.github.com>
2022-04-25 10:23:27 +0530

    Add files via upload

---
+++ practicedemo4.py
@@ -0,0 +1,34 @@
+# import sys
+# from datetime import date
+# from math import pi
+# print(sys.version)
+# print(date.today())
+
+# area of a circle
+#radius = float(input("radius = "))
+#area = (radius*radius*pi)
+#print("The area of the circle with radius "+str(radius)+"="+str(area))
+
+# nos = input("Geben sie zahlen durch kommas getrrent: ")
+#
+# list1 = list(nos.split(","))
+#
+# tuple1 = tuple(list1)
+#
+# print(list1)
+#
+# print(tuple1)
+
+#accepting a filename from the user and printing the extension of the file
+#
+# file = input("Bitte geben sie den dateinamen sie ein->..")
+#
+# ext = file.split(".", [-1])
+#
+# print(ext)
+
+farben = ["rot","blau","Grun","lila"]
+
+print(farben[0])
+print(farben[-1])
+
