c5ff81976318113d7ecc5cd48654f8f974332cf3
Som <94239450+fortheloveofcoding99@users.noreply.github.com>
2022-05-04 11:32:07 +0530

    Add files via upload

---
+++ practiceDemo7.py
@@ -0,0 +1,57 @@
+
+
+# str = input("Enter String:")
+#
+# times = int(input("No.of times:"))
+#
+# if times < 2:
+#     print(str[0:len(str)]*times)
+# else:
+#     print(str[0:2]*times)
+
+# splitted_string = str[0:2]
+#
+# print(splitted_string*times)
+
+# #Checking a letter in the list is vowel
+#
+# char = input("character:")
+#
+# vowels_list = ['a','e','i','o','u']
+#
+# if char.lower() in vowels_list:
+#     print("Vowel present")
+# else:
+#     print("its consonant")
+
+# #check if the number is present in the list
+#
+# list_Numbers = input("Enter the list:").split(',')
+# check_number = input("Checking number:")
+#
+# list_Num = list(list_Numbers)
+
+# for i in list_Num:
+#     if i == check_number:
+#         print("number found")
+#         break
+# else:
+#     print("number not found")
+#
+# if check_number in list_Num:
+#     print("true")
+# else:
+#     print("false")
+
+# #takes a sequence of numbers and find if the numbers are different from each other
+#
+# ln = input("Enter the list of numbers separated by commas eg. 1,2,3,4 ->").split(',')
+# listNumbers = list(ln)
+
+# if len(listNumbers) == len(set(listNumbers)):
+#     print("All unique numbers")
+# else:
+#     print("duplicate numbers in the list")
+
+
+
